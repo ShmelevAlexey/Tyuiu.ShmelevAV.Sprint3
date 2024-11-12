ffd57a75becb537e5143a63a34033e265dff837f
Алексей Шмелев <Tyuiu.shmelevav@outlook.com>
2024-11-12 21:56:21 +0500

    Task6

---
+++ Tyuiu.ShmelevAV.Sprint3.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Tyuiu.ShmelevAV.Sprint3.Task6.V4.Lib;
+
+namespace Tyuiu.ShmelevAV.Sprint3.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 14;
+            int stopValue = 20;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 0;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShmelevAV.Sprint3.Task6.V4/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.ShmelevAV.Sprint3.Task6.V4.Lib;
+
+namespace Tyuiu.ShmelevAV.Sprint3.Task6.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Шмелев А. В. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Шмелев Алексей Витальевич | ИИПб-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [14, 20] количество всех делителей больше 9           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 14;
+            int stopValue = 20;
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
